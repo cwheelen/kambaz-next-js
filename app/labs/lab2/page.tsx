69955952be37b2494271afd22769e72e88fbf17d
cwheelen <113392105+cwheelen@users.noreply.github.com>
2026-02-06 18:37:25 -0500

    added name

--- app/labs/lab2/page.tsx
+++ app/labs/lab2/page.tsx
@@ -26,6 +26,7 @@ export default function Lab2() {
   return (
     
     <Container>
+      <h1>Christopher Wheelen</h1>
       <h2>Lab 2 - Cascading Style Sheets</h2>
       <h3>Styling with the STYLE attribute</h3>
       <p>
