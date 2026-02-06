315584b91173ffc62e9fb1d4cbee1d44ae006625
cwheelen <113392105+cwheelen@users.noreply.github.com>
2026-02-05 20:04:07 -0500

    finished 2.5.4

--- app/(kambaz)/dashboard/page.tsx
+++ app/(kambaz)/dashboard/page.tsx
@@ -1,29 +1,60 @@
 import Link from "next/link";
-import Image from "next/image";
+import { Row, Col, Card, Button } from "react-bootstrap";
+
 export default function Dashboard() {
   return (
     <div id="wd-dashboard">
       <h1 id="wd-dashboard-title">Dashboard</h1> <hr />
       <h2 id="wd-dashboard-published">Published Courses (12)</h2> <hr />
       <div id="wd-dashboard-courses">
-        <div className="wd-dashboard-course">
-          <Link href="/courses/1234" className="wd-dashboard-course-link">
-            <Image src="/images/reactjs.jpg" width={200} height={150} alt="reactjs" />
-            <div>
-              <h5> CS1234 React JS </h5>
-              <p className="wd-dashboard-course-title">
-                Full Stack software developer
-              </p>
-              <button> Go </button>
-            </div>
-          </Link>
-        </div>
-        <div className="wd-dashboard-course"> game 4700 - game capstone </div>
-        <div className="wd-dashboard-course"> programming in c++ </div>
-        <div className="wd-dashboard-course"> writing 101 </div>
-        <div className="wd-dashboard-course"> intro to communications </div>
-        <div className="wd-dashboard-course"> narrative for games </div>
-        <div className="wd-dashboard-course"> chinese tea ceremonies </div>
+        <Row xs={1} md={5} className="g-4">
+          <Col className="wd-dashboard-course" style={{ width: "300px" }}>
+            <Card>
+              <Link href="/courses/1234/home" className="wd-dashboard-course-link text-decoration-none text-dark">
+                <img 
+                  src="/images/reactjs.jpg" 
+                  className="card-img-top"
+                  alt="React JS"
+                  style={{ height: "160px", objectFit: "cover" }}
+                />
+                <div className="card-body">
+                  <h5 className="card-title wd-dashboard-course-title text-nowrap overflow-hidden">
+                    CS1234 React JS
+                  </h5>
+                  <p className="card-text wd-dashboard-course-description overflow-hidden" style={{ height: "100px" }}>
+                    Full Stack software developer
+                  </p>
+                  <Button variant="primary">Go</Button>
+                </div>
+              </Link>
+            </Card>
+          </Col>
+          
+          <Col className="wd-dashboard-course" style={{ width: "300px" }}>
+            <div>GAME 4700 - game capstone</div>
+          </Col>
+          
+          <Col className="wd-dashboard-course" style={{ width: "300px" }}>
+            <div>Programming in C++</div>
+          </Col>
+          
+          <Col className="wd-dashboard-course" style={{ width: "300px" }}>
+            <div>Writing 101</div>
+          </Col>
+          
+          <Col className="wd-dashboard-course" style={{ width: "300px" }}>
+            <div>Intro to Communications</div>
+          </Col>
+          
+          <Col className="wd-dashboard-course" style={{ width: "300px" }}>
+            <div>Narrative for Games</div>
+          </Col>
+          
+          <Col className="wd-dashboard-course" style={{ width: "300px" }}>
+            <div>Chinese Tea Ceremonies</div>
+          </Col>
+        </Row>
       </div>
     </div>
-);}
+  );
+}
