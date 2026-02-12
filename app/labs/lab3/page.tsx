c91f93cd57d8b138b66320d6d238487bec113c2d
cwheelen <113392105+cwheelen@users.noreply.github.com>
2026-02-11 20:42:34 -0500

    3.3 done

--- app/labs/lab3/page.tsx
+++ app/labs/lab3/page.tsx
@@ -1,6 +1,30 @@
+import VariablesAndConstants from "./VariablesAndConstants";
+import VariableTypes from "./VariableTypes";
+import BooleanVariable from "./BooleanVariables";
+import IfElse from "./IfElse";
+import TernaryOperator from "./TernaryOperator";
+import ConditionalOutputIfElse from "./ConditionalOutputIfElse";
+import ConditionalOutputInline from "./ConditionalOutputInline";
+import LegacyFunctions from "./LegacyFunctions";
+import ArrowFunctions from "./ArrowFunctions";
+import ImpliedReturn from "./ImpliedReturn";
+import TemplateLiterals from "./TemplateLiterals";
+
 export default function Lab3() {
-  return (
-    <div>
-      <h2>Lab 3</h2>
+  return(
+    <div id="wd-lab3">
+      <h3>Lab 3</h3>
+      <VariablesAndConstants/>
+      <VariableTypes />
+      <BooleanVariable />
+      <IfElse />
+      <TernaryOperator />
+      <ConditionalOutputIfElse />
+      <ConditionalOutputInline />
+      <LegacyFunctions />
+      <ArrowFunctions />
+      <ImpliedReturn />
+      <TemplateLiterals />
     </div>
-);}
+  );
+}
