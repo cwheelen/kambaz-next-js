024d840ce04e9650171d75db31af08a17c94c354
cwheelen <113392105+cwheelen@users.noreply.github.com>
2026-02-17 19:42:39 -0500

    first half of 3.10 done

--- app/(kambaz)/Navigation.tsx
+++ app/(kambaz)/Navigation.tsx
@@ -1,91 +1,42 @@
-import Link from "next/link";
-import { ListGroup } from "react-bootstrap";
-import { FaRegCircleUser, FaInbox, FaFlask } from "react-icons/fa6";
+"use client"
 import { AiOutlineDashboard } from "react-icons/ai";
 import { IoCalendarOutline } from "react-icons/io5";
-import { LiaBookSolid } from "react-icons/lia";
-
+import { LiaBookSolid, LiaCogSolid } from "react-icons/lia";
+import { FaInbox, FaRegCircleUser } from "react-icons/fa6";
+import { usePathname } from "next/navigation";
+import Link from "next/link";
+import { ListGroup, ListGroupItem } from "react-bootstrap";
 export default function KambazNavigation() {
+  const pathname = usePathname();
+  const links = [
+    { label: "Dashboard", path: "/dashboard", icon: AiOutlineDashboard },
+    { label: "Courses",   path: "/dashboard", icon: LiaBookSolid },
+    { label: "Calendar",  path: "/Calendar",  icon: IoCalendarOutline },
+    { label: "Inbox",     path: "/Inbox",     icon: FaInbox },
+    { label: "Labs",      path: "/labs",             icon: LiaCogSolid },
+  ];
   return (
-    <ListGroup 
-      className="rounded-0 position-fixed bottom-0 top-0 d-none d-md-block bg-black z-2" 
-      style={{ width: 120 }}
-      id="wd-kambaz-navigation"
-    >
-      <Link 
-        className="list-group-item bg-black border-0 text-center text-decoration-none" 
-        target="_blank" 
-        href="https://www.northeastern.edu/" 
-        id="wd-neu-link"
-      >
-        <img src="/images/NEU.png" width="75px" alt="Northeastern University" />
-      </Link>
-      <br />
-      
-      <Link 
-        href="/account" 
-        id="wd-account-link" 
-        className="list-group-item border-0 bg-black text-center text-white text-decoration-none"
-      >
-        <FaRegCircleUser className="fs-1 text-white" />
+    <ListGroup id="wd-kambaz-navigation" style={{width: 120}}
+         className="rounded-0 position-fixed bottom-0 top-0 d-none d-md-block bg-black z-2">
+      <ListGroupItem id="wd-neu-link" target="_blank" href="https://www.northeastern.edu/"
+        action className="bg-black border-0 text-center">
+        <img src="/images/NEU.png" width="75px" /></ListGroupItem>
+      <ListGroupItem as={Link} href="/account"
+        className={`text-center border-0 bg-black
+            ${pathname.includes("Account") ? "bg-white text-danger" : "bg-black text-white"}`}>
+        <FaRegCircleUser
+          className={`fs-1 ${pathname.includes("Account") ? "text-danger" : "text-white"}`} />
         <br />
         Account
-      </Link>
-      <br />
-      
-      <Link 
-        href="/dashboard" 
-        id="wd-dashboard-link" 
-        className="list-group-item border-0 bg-white text-center text-danger text-decoration-none"
-      >
-        <AiOutlineDashboard className="fs-1 text-danger" />
-        <br />
-        Dashboard
-      </Link>
-      <br />
-      
-      <Link 
-        href="/dashboard" 
-        id="wd-course-link" 
-        className="list-group-item border-0 bg-black text-center text-white text-decoration-none"
-      >
-        <LiaBookSolid className="fs-1 text-white" />
-        <br />
-        Courses
-      </Link>
-      <br />
-      
-      <Link 
-        href="/calendar" 
-        id="wd-calendar-link" 
-        className="list-group-item border-0 bg-black text-center text-white text-decoration-none"
-      >
-        <IoCalendarOutline className="fs-1 text-white" />
-        <br />
-        Calendar
-      </Link>
-      <br />
-      
-      <Link 
-        href="/inbox" 
-        id="wd-inbox-link" 
-        className="list-group-item border-0 bg-black text-center text-white text-decoration-none"
-      >
-        <FaInbox className="fs-1 text-white" />
-        <br />
-        Inbox
-      </Link>
-      <br />
-      
-      <Link 
-        href="/labs" 
-        id="wd-labs-link" 
-        className="list-group-item border-0 bg-black text-center text-white text-decoration-none"
-      >
-        <FaFlask className="fs-1 text-white" />
-        <br />
-        Labs
-      </Link>
+      </ListGroupItem>
+      {links.map((link) => (
+        <ListGroupItem key={link.path} as={Link} href={link.path}
+          className={`bg-black text-center border-0
+            ${pathname.includes(link.label) ? "text-danger bg-white":"text-white bg-black"}`}>
+          {link.icon({ className: "fs-1 text-danger"})}
+          <br />
+          {link.label}
+        </ListGroupItem>
+      ))}
     </ListGroup>
-  );
-}
+);}
--- app/(kambaz)/dashboard/page.tsx
+++ app/(kambaz)/dashboard/page.tsx
@@ -1,60 +1,32 @@
+/* eslint-disable react/jsx-key */
 import Link from "next/link";
-import { Row, Col, Card, Button } from "react-bootstrap";
-
+import * as db from "../database";
+import { Button, Card, CardBody, CardImg, CardText, CardTitle, Col, Row } from "react-bootstrap";
 export default function Dashboard() {
-  return (
-    <div id="wd-dashboard">
-      <h1 id="wd-dashboard-title">Dashboard</h1> <hr />
-      <h2 id="wd-dashboard-published">Published Courses (12)</h2> <hr />
-      <div id="wd-dashboard-courses">
-        <Row xs={1} md={5} className="g-4">
-          <Col className="wd-dashboard-course" style={{ width: "300px" }}>
-            <Card>
-              <Link href="/courses/1234/home" className="wd-dashboard-course-link text-decoration-none text-dark">
-                <img 
-                  src="/images/reactjs.jpg" 
-                  className="card-img-top"
-                  alt="React JS"
-                  style={{ height: "160px", objectFit: "cover" }}
-                />
-                <div className="card-body">
-                  <h5 className="card-title wd-dashboard-course-title text-nowrap overflow-hidden">
-                    CS1234 React JS
-                  </h5>
-                  <p className="card-text wd-dashboard-course-description overflow-hidden" style={{ height: "100px" }}>
-                    Full Stack software developer
-                  </p>
-                  <Button variant="primary">Go</Button>
-                </div>
-              </Link>
-            </Card>
-          </Col>
-          
-          <Col className="wd-dashboard-course" style={{ width: "300px" }}>
-            <div>GAME 4700 - game capstone</div>
-          </Col>
-          
-          <Col className="wd-dashboard-course" style={{ width: "300px" }}>
-            <div>Programming in C++</div>
-          </Col>
-          
-          <Col className="wd-dashboard-course" style={{ width: "300px" }}>
-            <div>Writing 101</div>
-          </Col>
-          
-          <Col className="wd-dashboard-course" style={{ width: "300px" }}>
-            <div>Intro to Communications</div>
-          </Col>
-          
-          <Col className="wd-dashboard-course" style={{ width: "300px" }}>
-            <div>Narrative for Games</div>
-          </Col>
-          
-          <Col className="wd-dashboard-course" style={{ width: "300px" }}>
-            <div>Chinese Tea Ceremonies</div>
-          </Col>
-        </Row>
-      </div>
-    </div>
-  );
-}
+ const courses = db.courses;
+ return (
+  <div id="wd-dashboard">
+   <h1 id="wd-dashboard-title">Dashboard</h1> <hr />
+   <h2 id="wd-dashboard-published">Published Courses ({courses.length})</h2> <hr />
+   <div id="wd-dashboard-courses">
+    <Row xs={1} md={5} className="g-4">
+     {courses.map((course) => (
+     <Col className="wd-dashboard-course" style={{ width: "300px" }}>
+      <Card>
+       <Link href={`/courses/${course._id}/home`}
+        className="wd-dashboard-course-link text-decoration-none text-dark" >
+        <CardImg src="/images/reactjs.jpg" variant="top" width="100%" height={160} />
+        <CardBody className="card-body">
+         <CardTitle className="wd-dashboard-course-title text-nowrap overflow-hidden">
+          {course.name} </CardTitle>
+         <CardText className="wd-dashboard-course-description overflow-hidden" style={{height:"100px"}}>
+          {course.description} </CardText>
+         <Button variant="primary"> Go </Button>
+        </CardBody>
+       </Link>
+      </Card>
+     </Col>
+    ))}
+   </Row>
+  </div>
+ </div>);}
