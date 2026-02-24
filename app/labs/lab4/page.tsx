9bb8b418b6f8208cc8a5dd58aa2480ba3679393c
cwheelen <113392105+cwheelen@users.noreply.github.com>
2026-02-24 16:56:52 -0500

    4.4 finished

--- app/labs/lab4/page.tsx
+++ app/labs/lab4/page.tsx
@@ -2,17 +2,42 @@
 import PassingFunctions from "./PassingFunctions";
 import ClickEvent from "./ClickEvent";
 import PassingDataOnEvent from "./PassingDataOnEvent";
+import Counter from "./Counter";
+import BooleanStateVariables from "./BooleanStateVariables";
+import StringStateVariables from "./StringStateVariables";
+import DateStateVariable from "./DateStateVariable";
+import ObjectStateVariable from "./ObjectStateVariable";
+import ArrayStateVariable from "./ArrayStateVariable";
+import ParentStateComponent from "./ParentStateComponent";
+import Link from "next/link";
+import store from "./store";
+import { Provider } from "react-redux";
+
+
 
 export default function Lab4() {
   function sayHello() {
     alert("Hello");
   }
   return (
-    <div id="wd-passing-functions">
-      <h2>Lab 4</h2>
-      
-      <PassingDataOnEvent />
-      <ClickEvent />
-      <PassingFunctions theFunction={sayHello} />
-    </div>
+    <Provider store={store}>
+      <div id="wd-passing-functions">
+        <h2>Lab 4</h2>
+        
+        <PassingDataOnEvent />
+        <ClickEvent />
+        <PassingFunctions theFunction={sayHello} />
+        <Counter />
+        <BooleanStateVariables />
+        <StringStateVariables />
+        <DateStateVariable />
+        <ObjectStateVariable />
+        <ArrayStateVariable />
+        <ParentStateComponent />
+        <Link href="/labs/lab4/redux">Redux Examples</Link>
+        <br />
+        <Link href="./lab4/react-context">React Context Examples</Link>
+
+      </div>
+    </Provider>
 );}
